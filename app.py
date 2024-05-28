c4d54a9101fb927a4c307fff5e39573b8ecae3fc
duncan <milkbear0313@gmail.com>
2024-05-28 04:49:59 +0000

    init commit

---
+++ app.py
@@ -0,0 +1,125 @@
+from datetime import datetime
+from flask import (
+    Flask,
+    request,
+    render_template,
+    render_template_string,
+    url_for,
+)
+from markupsafe import escape
+import os
+from infererence import get_hooked_model, get_htmls
+
+app = Flask(__name__)
+
+# Ensure the 'generated_html' directory exists
+if not os.path.exists("generated_html"):
+    os.makedirs("generated_html")
+
+# Load models
+models = {
+    "gpt2": get_hooked_model("gpt2"),
+    "gpt2-large": get_hooked_model("gpt2-large"),
+    "codellama-7b-python": get_hooked_model("codellama/CodeLlama-7b-Python-hf"),
+}
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/generate_html", methods=["POST"])
+def generate_html():
+    prompt = request.form["prompt"]
+    model_name = request.form["model"]
+    sanitized_prompt = prompt
+    sanitized_model_name = escape(model_name)
+
+    # Generate text using the selected model
+    hooked_model = models.get(model_name)
+    htmls = get_htmls(sanitized_prompt, hooked_model)
+
+    # Simulate multiple HTML generation
+    gen_path = "generated_html"
+    save_path = f"old_versions/{datetime.now().strftime(f'%Y-%m-%d_%H-%M-%S')}"
+    os.makedirs(save_path, exist_ok=True)
+    generated_files = []
+    for i, generated_text in enumerate(htmls):
+        file_name = f"Layer_{i}.html"
+        # Create dir for save_path
+        html_content = generated_text
+
+        gen_path_full = os.path.join(gen_path, file_name)
+        save_path_full = os.path.join(save_path, file_name)
+
+        with open(gen_path_full, "w") as f:
+            f.write(html_content)
+        with open(save_path_full, "w") as f:
+            f.write(html_content)
+
+        generated_files.append(file_name)
+
+    # Generate buttons for each HTML file
+    buttons_html = "".join(
+        [
+            f'<div class="button-container"><a href="{url_for("serve_html", filename=filename)}"><button>{filename}</button></a></div>'
+            for filename in generated_files
+        ]
+    )
+
+    return render_template_string(
+        f"<html><head><link rel='stylesheet' href='/static/styles/home.css'></head><body class='styled-body'>{buttons_html}</body></html>"
+    )
+
+
+@app.route("/generated_html/<filename>")
+def serve_html(filename):
+    file_path = os.path.join("generated_html", filename)
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+@app.route("/old_versions", methods=["GET"])
+def old_versions():
+    dir_list = os.listdir("old_versions")
+    atag_htmls = "".join(
+        [
+            f'<div class="button-container"><a href="{url_for("serve_old_version", filename=filename)}"><button>{filename}</button></a></div>'
+            for filename in dir_list
+        ]
+    )
+    return render_template_string(
+        f"<html><head><link rel='stylesheet' href='/static/styles/home.css'></head><body class='styled-body'><h1>Old Versions</h1>{atag_htmls}</body></html>"
+    )
+
+
+@app.route("/old_versions/<filename>")
+def serve_old_version(filename):
+    file_path = os.path.join("old_versions", filename)
+    file_list = os.listdir(file_path)
+
+    def find_layer(file_name):
+        return int(file_name.split("_")[1].split(".")[0])
+
+    file_list = sorted(file_list, key=find_layer)
+    atag_htmls = "".join(
+        [
+            f'<div class="button-container"><a href="{filename}/{file}"><button>{file}</button></a></div>'
+            for file in file_list
+        ]
+    )
+    return render_template_string(
+        f"<html><head><link rel='stylesheet' href='/static/styles/home.css'></head><body class='styled-body'><h1>{filename}</h1>{atag_htmls}</body></html>"
+    )
+
+
+@app.route("/old_versions/<filename>/<file>")
+def serve_old_version_file(filename, file):
+    file_path = os.path.join("old_versions", filename, file)
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ infererence.py
@@ -0,0 +1,59 @@
+from typing import List
+from transformer_lens import HookedTransformer, ActivationCache
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch as t
+from AttenScope import AttentionPostprocessing, TensorToHeatmap
+
+device = t.device("cpu")
+previous_model = None
+
+
+def get_hooked_model(model_name: str) -> HookedTransformer:
+    global device
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = model.to(device)
+    hooked_model = HookedTransformer.from_pretrained_no_processing(
+        model_name=model_name,
+        hf_model=model,
+        tokenizer=tokenizer,
+        device=device,
+        dtype="float16",
+    )
+    return hooked_model
+
+
+def get_htmls(prompt: str, hooked_model: HookedTransformer) -> List[str]:
+    assert isinstance(prompt, str)
+    assert isinstance(hooked_model, HookedTransformer)
+
+    global previous_model
+
+    print("Prompt:", prompt)
+    if previous_model != hooked_model:
+        if previous_model is not None:
+            previous_model = previous_model.to("cpu")
+        previous_model = hooked_model
+        hooked_model = hooked_model.to("cuda")
+    toks = hooked_model.to_tokens(prompt)
+    str_toks = hooked_model.to_str_tokens(toks)
+    _, cache = hooked_model.run_with_cache(toks)
+    cache = cache.to("cpu")
+    print("Str tokens:", str_toks)
+
+    htmls = []
+    num_of_layer = hooked_model.cfg.n_layers
+    for layer in range(num_of_layer):
+        attention = cache["pattern", layer].squeeze()
+        value = cache["v", layer].squeeze().transpose(0, 1)
+        post_processed_attention = [
+            AttentionPostprocessing.apply_value_norm_to_attention(a, v).numpy()
+            for a, v in zip(attention, value)
+        ]
+
+        labels = [f"Head {head}" for head in range(attention.size(0))]
+        html = TensorToHeatmap.generate_heatmap_html(
+            post_processed_attention, labels, str_toks
+        )
+        htmls.append(html)
+    return htmls
